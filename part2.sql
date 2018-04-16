85e7c8b8c8633929ff9d7c331a071e7717e112d6
Vincent <wenghchn@outlook.com>
2018-04-16 13:19:26 -0400

    Finish part 2 queries and .tex report

--- part2.sql
+++ part2.sql
@@ -1,21 +1,28 @@
 DROP USER IF EXISTS 'user1'@'%';
 CREATE USER user1;
-GRANT SELECT ON yelp_db.* TO 'user1'@'%'; <-- This user only need to view data from the database. -->
+GRANT SELECT ON yelp_db.* TO 'user1'@'%';
 
 DROP USER IF EXISTS 'user2'@'%';
 CREATE USER user2;
-GRANT SELECT, INSERT ON yelp_db.* TO 'user2'@'%'; <-- This user leaves review, therefore needs insert privilege. -->
+GRANT SELECT ON yelp_db.* TO 'user2'@'%';
+GRANT INSERT ON yelp_db.review TO 'user2'@'%';
+GRANT INSERT ON yelp_db.tip TO 'user2'@'%';
+GRANT UPDATE (stars) ON yelp_db.business TO 'user2'@'%';
+GRANT UPDATE (review_count) ON yelp_db.business TO 'user2'@'%';
+GRANT UPDATE ON yelp_db.user TO 'user2'@'%';
 
 DROP USER IF EXISTS 'user3'@'%';
 CREATE USER user3;
-GRANT SELECT, CREATE VIEW, SHOW VIEW ON yelp_db.* TO 'user3'@'%'; <-- This user needs to create and see views to do data analysis. -->
+GRANT SELECT, CREATE VIEW, SHOW VIEW ON yelp_db.* TO 'user3'@'%';
 
 DROP USER IF EXISTS 'user4'@'%';
 CREATE USER user4;
-GRANT ALTER, ALTER ROUTINE, CREATE, CREATE ROUTINE, CREATE VIEW, DELETE, DROP, EXECUTE, INDEX, INSERT, REFERENCES,
-      SELECT, SHOW VIEW, UPDATE ON yelp_db.* TO 'user4'@'%'; 
-      <-- This user can do IUD operations, create/execute/see routines including functionsa and procedures, altering table to add keys/indices -->
+GRANT ALTER ROUTINE, CREATE ROUTINE, EXECUTE, # routine related
+CREATE VIEW, SHOW VIEW, # view related
+CREATE, ALTER, INDEX, REFERENCES, # tables, indexes and keys
+DELETE, DROP, INSERT, SELECT, UPDATE # basic operations including IUD
+ON yelp_db.* TO 'user4'@'%';
 
 DROP USER IF EXISTS 'user5'@'%';
 CREATE USER user5;
-GRANT ALL ON yelp_db.* TO 'user5'@'%'; <-- This is the admin of database. ALL privilege granted. -->
+GRANT ALL ON yelp_db.* TO 'user5'@'%';
