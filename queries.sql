455c0554b27e98c2c42cffcbd720f67d1158223f
jsreid13 <js2reid@uwaterloo.ca>
2018-04-19 19:02:28 -0400

    Added additional explaination to analysis

--- queries.sql
+++ queries.sql
@@ -50,9 +50,9 @@ create table hours_new(
     ,closing_time time
     );
     
-drop procedure dowhile;
+drop procedure dowhile_hours;
 delimiter //
-create procedure dowhile ()
+create procedure dowhile_hours ()
 begin declare v1 int default 821044;
 while v1 > 0 do
 insert into hours_new(id) values (null);
@@ -60,8 +60,10 @@ set v1 = v1 - 1;
 end while;
 end//
 delimiter ;
+   
 
-call dowhile();
+
+call dowhile_hours();
 
 update hours_new set day_of_week = (
 	select SUBSTRING_INDEX(hours, '|', 1) from hours where hours_new.id = hours.id);
@@ -71,3 +73,29 @@ update hours_new set closing_time = (
 	select SUBSTRING_INDEX(SUBSTRING_INDEX(hours, '|', - 1), '-', - 1) from hours where hours_new.id = hours.id);
 update hours_new set business_id = (
 	select business_id from hours where hours_new.id = hours.id);
+
+create index idx_day_of_week on hours_new(day_of_week);
+create index idx_opening_time on hours_new(opening_time);
+create index idx_closing_time on hours_new(closing_time);
+
+drop procedure dowhile_checkin;
+delimiter //
+create procedure dowhile_checkin()
+begin declare v1 int default 3891600;
+while v1 > 0 do
+insert into checkin_new(id) values (null);
+set v1 = v1 - 1;
+end while;
+end//
+delimiter ;
+
+call dowhile_checkin();
+
+update checkin_new set day_of_week = (
+	select SUBSTRING_INDEX(date, '-', 1) from checkin where checkin_new.id = checkin.id);
+update checkin_new set checkin_time = (
+	select SUBSTRING_INDEX(date, '-', -1) from checkin where checkin_new.id = checkin.id);
+update checkin_new set business_id = (
+	select business_id from checkin where checkin_new.id = checkin.id);
+create index idx_day_of_week on checkin_new(day_of_week);
+create index idx_opening_time on checkin_new(checkin_time);
